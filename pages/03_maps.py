e2ecb541deb971b8173989d6f1177eb38fa95257
Piotr Derewonko <piotr.derewonko@outlook.com>
2026-05-24 13:22:56 +0200

    poprawa mapy

--- pages/03_maps.py
+++ pages/03_maps.py
@@ -4,6 +4,7 @@ import os
 import pandas as pd
 
 with st.container(width=1900, horizontal_alignment='left'):
+    st.title('Mapa wypadków drogowych w dzielnicach Nowego Yorku')
     path_to_csv = os.path.join(os.getcwd(), Path(__file__).parent.parent, "data\\dane_przetworzone.csv")
 
     borough_list = ['BRONX',
@@ -12,9 +13,20 @@ with st.container(width=1900, horizontal_alignment='left'):
                     'QUEENS',
                     'STATEN ISLAND']
     borough = st.multiselect('Wybierz dzielnicę', borough_list, default=borough_list)
+    count_of_accident = st.slider(min_value=0, max_value=533, value=[0, 533],
+                                  label='Wybierz zakres ilośc wypadków w jednym miejscu')
+    st.markdown(count_of_accident)
 
     reload = st.button('Załaduj dane')
     if reload:
         data = pd.read_csv(path_to_csv, low_memory=False)
+        total_accidents = len(data)
         data = data.loc[data['BOROUGH'].isin(borough)]
-        st.map(data.loc[data['brak_wspolrzednych'] == False])
+        data = data.loc[(data['ile_wypadkow_w_danym_miejscu'] >= count_of_accident[0]) & (
+                data['ile_wypadkow_w_danym_miejscu'] <= count_of_accident[1])]
+        filtered_accidents = len(data)
+        st.markdown(
+            f'Znaleziono {filtered_accidents} z {total_accidents} wszystkich wypadków co stanowi {filtered_accidents / total_accidents * 100:.2f}% wszystkich wypadków')
+        st.map(data.loc[data['brak_wspolrzednych'] == False],height=650)
+        with st.expander(label='Dane Tabelaryczne'):
+            st.dataframe(data)
--- report_app.py
+++ report_app.py
@@ -1,9 +1,16 @@
 import streamlit as st
 
-def app():
-    st.set_page_config(layout="wide")
-    st.title("Praca Dyplomowa Piotr Derewońko ALK")
+
+st.set_page_config(layout="wide")
+pages = {
+        "Raporty": [
+            st.Page("pages/02_reason.py", title="Ilość wypadków"),
+            st.Page("pages/03_maps.py", title="Mapy"),
+
+        ],
+
+    }
+pg = st.navigation(pages, position="sidebar")
+pg.run()
 
 
-if __name__ == "__main__":
-    app()
